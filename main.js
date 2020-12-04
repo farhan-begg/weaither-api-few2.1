a91af679c509201713dd35e2465995f6a0096655
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-12-03 17:14:03 -0800

    update

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
-import getWeather from './weather.js';
-import onWeather from './weather.js'
+import {getWeather} from './weather.js';
 
-const tempEl = document.getElementById('temp')
-const descEl = document.getElementById('desc')
-const formEl = document.getElementById('form')
-const zipInput = document.getElementById('zip')
+        const tempEl = document.getElementById('temp')
+        const descEl = document.getElementById('desc')
+        const formEl = document.getElementById('form')
+        const zipInput = document.getElementById('zip')
 
-// Define event listeners
-formEl.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const zip = zipInput.value
-    // zip ==== argument ==== value ==== '94122'
-    const p = getWeather(zip)
-    p.then(data => {
-        onWeather(data)
-    })
-})
+        // Define event listeners
+        formEl.addEventListener('submit', (e) => {
+            e.preventDefault()
+            const zip = zipInput.value
+            const apiKey = '467355df4c808dd6134a3b64e9ace282'
+            // zip ==== argument ==== value ==== '94122'
+            const p = getWeather(zip, apiKey)
+            p.then(data => {
+                onWeather(data)
+            })
+        })
 
-
+        function onWeather(data) {
+            temp.innerHTML = data.main.temp
+            desc.innerHTML = data.weather[0].description
+        }
--- weather.js
+++ weather.js
@@ -1,14 +1,12 @@
-       function onWeather(data) {
-        temp.innerHTML = data.main.temp
-        desc.innerHTML = data.weather[0].description
-    }
+    //    export function onWeather(data) {
+    //     temp.innerHTML = data.main.temp
+    //     desc.innerHTML = data.weather[0].description
+    // }
 
       // Functions 
-      function getWeather(zip) {
+     export function getWeather(zip, apiKey, units = 'imperial') {
         // const zip = '94122'
         // zip ==== parameter ==== variable 
-        const apiKey = '467355df4c808dd6134a3b64e9ace282'
-        const units = 'imperial'
         const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
         const p = fetch(path)
         const p2 = p.then(res => res.json())
